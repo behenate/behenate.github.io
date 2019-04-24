96450e43ba4b17d3ec5919d72bc966a2b1d79d17
Wojciech D <behenate@gmail.com>
2019-04-24 18:55:32 +0200

    Woodworking commit 2

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+anime({
+    targets:"#landing-arrow, .landing-title",
+    opacity: 0,
+    duration:0
+})
 var landingPageTl = anime.timeline({
     easing: 'easeInOutQuart',
     duration:1200,
@@ -37,10 +42,51 @@ landingPageTl
 document.querySelector('#landing-arrow').addEventListener('mouseover', () => anime({
         targets: "#landing-arrow",
         translateY: "10px",
-        duration:300   
+        duration:300,
+        begin: function(anim){
+            landingPageTl.pause();
+        }   
 }));
 document.querySelector('#landing-arrow').addEventListener('mouseout', () => anime({
     targets: "#landing-arrow",
     translateY: "0px",
     duration:300   
 }));
+
+let titles = document.querySelectorAll(".landing-title");
+let mainInTl = anime.timeline({
+    easing: 'easeInOutQuart',
+    autoplay:false
+});
+mainInTl
+.add({
+    targets: '.main-container',
+    top:"0vh",
+    duration:1800
+}, 400)
+.add({
+    targets:'#landing-arrow',
+    opacity:["1","0"],
+    translateY: ["10px","100px"],
+    rotate:["-45deg", "-45deg"],
+    duration: 400
+},0)
+.add({
+    targets:titles[0],
+    opacity:["1","0"],
+    translateX: "300px",
+    translateY:["0px", "0px"]
+},0)
+.add({
+    targets:titles[1],
+    opacity:["1","0"],
+    translateX: "-300px",
+    translateY:["0px", "0px"]
+},0)
+.add({
+    targets:".background-image",
+    top: "-30vh",
+    duration:1800
+},400)
+mainInTl.pause;
+document.querySelector('#landing-arrow').addEventListener('click', () => mainInTl.play());
